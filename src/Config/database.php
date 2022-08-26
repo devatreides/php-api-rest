9262db027e3317205cea7719d5755c4cd7f31b63
Tom Benevides <eltonfelixbenevides@gmail.com>
2022-08-26 14:55:48 -0400

    config: changing host value

--- src/Config/database.php
+++ src/Config/database.php
@@ -2,7 +2,7 @@
 
 return [
     'driver' => 'pdo_pgsql',
-    'host' => 'host.docker.internal',
+    'host' => 'localhost',
     'dbname' => 'db_api',
     'user' => 'postgres',
     'password' => '101320'
